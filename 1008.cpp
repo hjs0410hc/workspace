edcd50d67911c0b2e4c6c8589260af2019be5029
hjs0410 <35911602+hjs0410hc@users.noreply.github.com>
2022-01-12 23:58:58 +0900

    shit happens

---
+++ 1008.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long double a,b;
+
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(0);
+    cin>>a>>b;
+    cout << fixed<<setprecision(30)<<a/b;
+    
+    
+}
