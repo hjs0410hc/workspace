76b69e00a828c6e99ed61e55fe95e8568f1e132c
hjs0410 <35911602+hjs0410hc@users.noreply.github.com>
2022-01-21 22:53:42 +0900

    BackTracking Practice problems solved (S3s)

---
+++ source/15649.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n,m;
+int arr[9];
+int temp[9];
+bool visited[9];
+
+void DFS(int depth){
+    if(depth == m){
+        for(int i=0;i<m;i++){
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+        return;   
+    }
+    for(int i=1;i<=n;i++){
+        if(!visited[i]){
+            visited[i] = true;
+            arr[depth] = i;
+            DFS(depth+1);
+            visited[i] = false;
+        }
+    }
+}
+
+
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(0);
+    cin>>n>>m;
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+    DFS(0);
+}
